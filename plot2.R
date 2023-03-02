8a984abbdf2a73cdb7dcfe4b67390965125c8376
Tiago Sousa <111656082+Tiagosous1@users.noreply.github.com>
2023-03-02 22:16:46 +0000

    update

--- plot2.R
+++ plot2.R
@@ -1,3 +1,40 @@
+#library needed
+library(tidyverse)
+
+#set sys time
+Sys.setlocale("LC_TIME", "C")
+
+#loading data
+household_data <- read_csv2("household_power_consumption.txt")
+
+#combining colums of date and time
+household_data <- unite(household_data, "Date_time", 1:2, sep = " ")
+
+#convert character to time in the column "Date_time"
+household_data$Date_time <- strptime(household_data$Date_time, "%d/%m/%Y %H:%M:%S")
+household_data$Date_time <- as.POSIXct(household_data$Date_time)
+
+#convert character to number
+household_data$Global_active_power <- as.numeric(household_data$Global_active_power)
+household_data$Global_reactive_power <- as.numeric(household_data$Global_reactive_power)
+household_data$Voltage <- as.numeric(household_data$Voltage)
+household_data$Global_intensity <- as.numeric(household_data$Global_intensity)
+household_data$Sub_metering_1 <- as.numeric(household_data$Sub_metering_1)
+household_data$Sub_metering_2 <- as.numeric(household_data$Sub_metering_2)
+household_data$Sub_metering_3 <- as.numeric(household_data$Sub_metering_3)
+
+#Subset the data to 2007-02-01 and 2007-02-02
+household_clean <- household_data[household_data$Date_time >= "2007-02-01" & household_data$Date_time < "2007-02-03",]
+
+#remove Na data
+household_clean <- household_clean[!is.na(household_clean$Date_time),]
+
+#confirm that are no NA
+sum(is.na.data.frame(household_clean))
+
+#Remove old data for geting space in memory
+#rm(household_data)
+
 #creating plot 2
 plot(household_clean$Global_active_power~household_clean$Date_time, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 
